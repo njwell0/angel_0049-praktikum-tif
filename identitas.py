d3996a73af4d97c0a10d0af3d60763931c8b8661
njwell0 <s3kar.sabat@gmail.com>
2026-08-21 13:53:54 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,11 @@
+judul = "biodata" #ini judul data
+nama = "Archangela Girlani Sekar Sabat" #menyimpan nama
+nim = "2605060049" #menyimpan NIM
+TTL = "Magelang, 26 Januari 2008" #menyimpan TTL
+hobi = "menyanyi" #menyimpan hobi
+# menampilkan biodata
+print (judul) 
+print (nama)
+print (nim)
+print (TTL)
+print (hobi)
