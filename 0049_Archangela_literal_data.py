2a26f7c992527b57c19b78297c56b3a7e7b8c9ef
njwell0 <s3kar.sabat@gmail.com>
2026-08-28 14:27:31 +0700

    Add files via upload

---
+++ 0049_Archangela_literal_data.py
@@ -0,0 +1,36 @@
+nama = "Bailey Kanjenny" #menyimpan nama data string
+umur = 25 #menyimpan umur data integer
+berat_badan = 47.8 #menyimpan berat badan data float
+data_bool = 000
+#menampilkan data 1
+print (nama) 
+print (umur)
+print (berat_badan)
+print (data_bool)
+
+#mengubah data string ke integer
+angka_string = "123"
+data_int = int(123)
+#mengubah data float ke integer
+angka_float = 45.67
+data_int = int(45.67)
+#mengubah data integer ke float
+angka_integer = 89
+data_float = float(89)
+#mengubah data integer ke string
+angka_integer = 89
+data_str = str(89)
+#menampilkan data 2
+print (data_int, type(angka_string))
+print (data_int, type(angka_float))
+print (data_float, type(angka_integer))
+print (data_str, type(angka_integer))
+
+#input data user
+usia = int(input("usia:"))
+TB = float(input("TB:"))
+nama = input("nama:")
+#menampilkan data 3
+print (int, type(usia))
+print (float, type(TB))
+print (input, type(nama)) 
